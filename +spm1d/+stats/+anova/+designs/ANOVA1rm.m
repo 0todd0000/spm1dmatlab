7e516458226b80c447c686f9c419fc942c647755
Todd Pataky <0todd0@gmail.com>
2015-12-30 18:10:40 +0900

    Version 0.3.1.1  (2015.12.30)
    !!! anova1rm bug !!!
    Fixed a bug in one-way repeated-measures ANOVA.
    This bug caused FWHM (field smoothness) over-estimations in cases of one observation per subject;  the bug did not exist for more than one observation per subject, and it only affected spm1d.stats.anova1rm.
    
    Updated minor bug in the display of SPM inference objects
    Previous calls to “disp(spmi)”, where spmi is a 1D SPM inference object, displayed: “SPM{t} (0D) inference”.
    Now calls to “disp(spmi)” display: “SPM{t} inference”.

--- +spm1d/+stats/+anova/+designs/ANOVA1rm.m
+++ +spm1d/+stats/+anova/+designs/ANOVA1rm.m
@@ -16,6 +16,20 @@ classdef ANOVA1rm < spm1d.stats.anova.designs.Design
             self   = assemble(self);
             check_balanced(self)
         end
+        
+        function [only_single] = check_for_single_responses(self)
+            [A,S] = deal(self.A.A, self.S.A); %#ok<*PROP>
+            only_single = false;
+            for iA = 1:self.A.n
+                a = self.A.u(iA);
+                s = S( (A==a) );
+                if numel(unique(s)) == numel(s)
+                    only_single = true;
+                    warning('Only one observation per subject found.  Residuals and inference will be approximate. To avoid approximate residuals: (a) Add multiple observations per subject and per condition, and (b) ensure that all subjects and conditions have the same number of observations.')
+                    return
+                end
+            end
+        end
     end
     
     methods (Access = private)
--- +spm1d/+stats/anova1rm.m
+++ +spm1d/+stats/anova1rm.m
@@ -2,7 +2,11 @@ function [SPM] = anova1rm(Y, A, SUBJ)
 
 design = spm1d.stats.anova.designs.ANOVA1rm(A, SUBJ);
 model  = spm1d.stats.anova.LinearModel(Y, design.X);
-model  = model.fit();
+if (model.dim==1) && ( design.check_for_single_responses() )
+    model  = model.fit(  design.contrasts.C(1:3,:)  );
+else
+    model  = model.fit();
+end
 SPM    = model.aov(design.contrasts, design.f_terms);
 SPM    = SPM{1};
 
