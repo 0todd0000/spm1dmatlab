b0dd409d3282adfad3da24706eaf0ba00fe8dd0e
Todd Pataky <0todd0@gmail.com>
2016-01-21 14:12:18 +0100

    version M.0.3.3
    Changed “Y1” and “Y2” variables in ttest_paired.m to “YA” and “YB” to be consistent with two-sample t test variable names.

--- examples/stats1d/ex1d_ttest_paired.m
+++ examples/stats1d/ex1d_ttest_paired.m
@@ -5,11 +5,11 @@ clear;  clc
 
 %(0) Load dataset:
 dataset = spm1d.data.uv1d.tpaired.PlantarArchAngle();
-[Y1,Y2] = deal(dataset.YA, dataset.YB);
+[YA,YB] = deal(dataset.YA, dataset.YB);
 
 
 %(1) Conduct SPM analysis:
-spm       = spm1d.stats.ttest_paired(Y1, Y2);
+spm       = spm1d.stats.ttest_paired(YA, YB);
 spmi      = spm.inference(0.05, 'two_tailed', false, 'interp',true);
 disp(spmi)
 
@@ -18,9 +18,9 @@ disp(spmi)
 close all
 %%% plot mean and SD:
 figure
-spm1d.plot.plot_meanSD(Y1, 'color','k')
+spm1d.plot.plot_meanSD(YA, 'color','k')
 hold on
-spm1d.plot.plot_meanSD(Y2, 'color','r')
+spm1d.plot.plot_meanSD(YB, 'color','r')
 %%% plot SPM results:
 figure
 spmi.plot()
