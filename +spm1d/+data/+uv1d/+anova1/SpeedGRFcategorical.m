a5d7815b1c15ec502ac97de430c1da04428f32ab
Todd Pataky <0todd0@gmail.com>
2016-09-12 15:19:00 -0500

    Modified the SpeedGRFcategorical dataset to allow access to any of the 10 subjects’ data using “SpeedGRFcategorical(x)”, where x is an integer from 0 to 9.

--- +spm1d/+data/+uv1d/+anova1/SpeedGRFcategorical.m
+++ +spm1d/+data/+uv1d/+anova1/SpeedGRFcategorical.m
@@ -1,11 +1,16 @@
-function [self] = SpeedGRFcategorical()
+function [self] = SpeedGRFcategorical(varargin)
 self.cite = 'Pataky, T. C., Caravaggi, P., Savage, R., Parker, D., Goulermas, J., Sellers, W., & Crompton, R. (2008). New insights into the plantar pressure correlates of walking speed using pedobarographic statistical parametric mapping (pSPM). Journal of Biomechanics, 41(9), 1987?1994.';
+if nargin>0
+    subj  = varargin{1};
+else
+    subj  = 0;
+end
 dir0      = spm1d.data.get_matfile_path();
-fnameY    = fullfile(dir0, 'ex_grf_subj000.mat');
+fnameY    = fullfile(dir0, sprintf('ex_grf_subj%03d.mat',subj) );
 fnameX    = fullfile(dir0, 'ex_grf_speeds_cond.mat');
 self.Y    = getfield(load(fnameY), 'Y');
 X         = getfield(load(fnameX), 'x');
-self.A    = X(:,1);
+self.A    = X(:,subj+1);
 end
 
 
