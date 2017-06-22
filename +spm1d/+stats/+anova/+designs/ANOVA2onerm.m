e6bbd9acaa353ad7329868f6909239e0ecca7100
Todd Pataky <0todd0@gmail.com>
2017-06-22 10:50:06 +0900

    version M.0.4.5 (2017.06.22)
    Fixed a second bug in spm1d.stats.anova2onerm
    
    The ANOVA2onerm class’s check_balanced method was not overriding the parent class’s (ANOVA2rm’s) check_balanced method so I separated all ANOVA2onerm functionality to be safe.  Unbalanced designs supported if and only if there are different numbers of subjects for different levels of Factor A.

--- +spm1d/+stats/+anova/+designs/ANOVA2onerm.m
+++ +spm1d/+stats/+anova/+designs/ANOVA2onerm.m
@@ -4,17 +4,43 @@
 
 
 
-classdef ANOVA2onerm < spm1d.stats.anova.designs.ANOVA2rm
+classdef ANOVA2onerm <  spm1d.stats.anova.designs.Design
+    properties
+        B
+        S
+    end
+    
     methods
         function self = ANOVA2onerm(A, B, SUBJ)
-            self@spm1d.stats.anova.designs.ANOVA2rm(A, B, SUBJ)
+            self.A = spm1d.stats.anova.factors.Factor(A);
+            self.B = spm1d.stats.anova.factors.Factor(B);
             self.S = spm1d.stats.anova.factors.FactorNested(SUBJ, self.A);
+            self.J = self.A.J;
             self.effect_labels = {'Main A', 'Main B', 'Interaction AB'};
             self.term_labels = {'Intercept', 'A', 'B', 'S', 'AB', 'SB'};
             self.f_terms = {{'A','S'}, {'B','SB'}, {'AB','SB'}};
             self   = assemble(self);
             self.check_balanced()
         end
+        
+        function [only_single] = check_for_single_responses(self, dim)
+            [A,B,S] = deal(self.A.A, self.B.A, self.S.A); %#ok<*PROP>
+            only_single = false;
+            for iA = 1:self.A.n
+                for iB = 1:self.B.n
+                    [a,b] = deal( self.A.u(iA), self.B.u(iB) );
+                    s = S( (A==a) & (B==b) );
+                    if numel(unique(s)) == numel(s)
+                        only_single = true;
+                        if dim==1
+                            warning('Only one observation per subject found.  Residuals and inference will be approximate. To avoid approximate residuals: (a) Add multiple observations per subject and per condition, and (b) ensure that all subjects and conditions have the same number of observations.')
+                        end
+                        return
+                    end
+                end
+            end
+        end
+        
     end
     
     methods (Access = private)
@@ -41,6 +67,8 @@ classdef ANOVA2onerm < spm1d.stats.anova.designs.ANOVA2rm
         function check_balanced(self)
             if ~(self.B.balanced && self.S.balanced)
                 error('Design must be balanced.')
+%             elseif ~self.A.check_balanced(self.B)
+%                 error('Design must be balanced.')
             elseif ~self.S.check_balanced_rm(self.A)
                 error('Design must be balanced.')
             elseif ~self.S.check_balanced_rm(self.B)
