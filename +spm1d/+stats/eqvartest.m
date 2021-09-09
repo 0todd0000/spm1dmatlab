eb0f10861e26118fb93d13655598a6613733fc51
Todd Pataky <todd@todds-imac-pro.lan>
2021-09-09 18:00:40 +0900

    Update eqvartest.m

--- +spm1d/+stats/eqvartest.m
+++ +spm1d/+stats/eqvartest.m
@@ -1,6 +1,37 @@
-function [f,fcrit] = ttest(y0, y1, varargin)
+function [f,fcrit] = eqvartest(y0, y1, varargin)
 %__________________________________________________________________________
 % Copyright (C) 2021 Todd Pataky
+%
+%	F test for equal variance.
+%	
+%	This test was reported in Kowalski et al. (2021) and adapted from the
+%	MATAB script provided as supplementary material with that article.
+%	
+%	NOTE: The current implementation of this test does not follow the
+%   general spm1d.stats procedures. See details below.
+%
+%	References:
+%	
+%	F-test of equality of variances
+%	https://en.wikipedia.org/wiki/F-test_of_equality_of_variances
+%	
+%	Kowalski, E., Catelli, D. S., & Lamontagne, M. (2021). A waveform test
+%	for variance inequality, with a comparison of ground reaction force
+%	during walking in younger vs. older adults. Journal of Biomechanics
+%	127: 110657.
+%	https://doi.org/10.1016/j.jbiomech.2021.110657
+%
+%	Inputs:
+%	
+%	- y0 --- (J0 x Q) numpy array (dependent variable, one group)
+%	- y1 --- (J1 x Q) numpy array (dependent variable, one group)
+%	- alt --- alternative hypothesis: "unequal" (default) or "greater" ( var(y0) > var(y1) )
+%	- alpha --- Type I error rate (set this to 0 to bypass inference and return only the F statistic)
+%	
+%	Returns:
+%	
+%	- f --- (Q,) array containing the test statistic.
+%	- fcrit --- random field theory-corrrected critical f value (NaN if alpha=0)
 
     % parse input arguments:
     parser = inputParser;
@@ -46,7 +77,6 @@ function [f,fcrit] = ttest(y0, y1, varargin)
         else
             pcrit = alpha;
         end
-        [pcrit,efwhm]
         fcrit = spm1d.rft1d.f.isf(pcrit, df, Q, efwhm);
     end
 
